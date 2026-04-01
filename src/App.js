9fbe947ac9bacf243d84e073ef8cb01d5a34b84f
spartshubs-arch <spartshubs@gmail.com>
2026-04-02 03:06:31 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from "react-toastify";
